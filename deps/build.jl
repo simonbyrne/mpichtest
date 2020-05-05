4182d2bc514d6a075003c739e6fbce1d92be06fe
Simon Byrne <simonbyrne@gmail.com>
2020-05-05 12:53:37 -0700

    print stuff

---
+++ deps/build.jl
@@ -0,0 +1,29 @@
+using MPICH_jll
+
+# work around SIP
+cp("/usr/bin/printenv","myprintenv")
+
+println("========================================")
+println("run(`./myprintenv`)")
+println("========================================")
+run(`./myprintenv`)
+
+println("========================================")
+println("mpiexec(cmd -> run(`./myprintenv`); adjust_LIBPATH=false)")
+println("========================================")
+mpiexec(cmd -> run(`./myprintenv`); adjust_LIBPATH=false)
+
+println("========================================")
+println("mpiexec(cmd -> run(`./myprintenv`))")
+println("========================================")
+mpiexec(cmd -> run(`./myprintenv`))
+
+println("========================================")
+println("mpiexec(cmd -> run(`$cmd -n 1 ./myprintenv`); adjust_LIBPATH=false)")
+println("========================================")
+mpiexec(cmd -> run(`$cmd -n 1 ./myprintenv`); adjust_LIBPATH=false)
+
+println("========================================")
+println("mpiexec(cmd -> run(`$cmd -n 1 ./myprintenv`))")
+println("========================================")
+mpiexec(cmd -> run(`$cmd -n 1 ./myprintenv`))
