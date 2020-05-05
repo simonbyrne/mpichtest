7ea8779d45fee11934a8a6d015044c2ba86bd008
Simon Byrne <simonbyrne@gmail.com>
2020-05-05 13:04:52 -0700

    try calling Julia

--- deps/build.jl
+++ deps/build.jl
@@ -27,3 +27,17 @@ println("========================================")
 println("mpiexec(cmd -> run(`\$cmd -n 1 ./myprintenv`))")
 println("========================================")
 mpiexec(cmd -> run(`$cmd -n 1 ./myprintenv`))
+
+
+println("========================================")
+println("mpiexec(cmd -> run(`julia --version`))")
+println("========================================")
+mpiexec(cmd -> run(`julia --version`))
+
+
+println("========================================")
+println("mpiexec(cmd -> run(`\$cmd -n 1 julia --version`))")
+println("========================================")
+mpiexec(cmd -> run(`$cmd -n 1 julia --version`))
+
+
