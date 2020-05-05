96e8dd0bc387b7f14e73b7fe4df419c339114c00
Simon Byrne <simonbyrne@gmail.com>
2020-05-05 13:22:01 -0700

    try

--- deps/build.jl
+++ deps/build.jl
@@ -29,15 +29,30 @@ println("========================================")
 mpiexec(cmd -> run(`$cmd -n 1 ./myprintenv`))
 
 
+paths = [
+    "/Users/travis/julia/bin/../lib/julia"
+    "/Users/travis/julia/bin/../lib"
+    "/Users/travis/.julia/artifacts/7c9ef733699a1d86b8a6073ed08a4457e3e790f7/lib"
+    "/Users/travis/.julia/artifacts/0fa85a44ee97edebd281cf5b65ff4f6009aaa15e/lib"
+]
+
+
+run(Cmd(`$(Base.julia_cmd()) --version`; env=Dict("DYLD_FALLBACK_LIBRARY_PATH"=>join(paths[1:1],':'))))
+run(Cmd(`$(Base.julia_cmd()) --version`; env=Dict("DYLD_FALLBACK_LIBRARY_PATH"=>join(paths[1:2],':'))))
+run(Cmd(`$(Base.julia_cmd()) --version`; env=Dict("DYLD_FALLBACK_LIBRARY_PATH"=>join(paths[1:3],':'))))
+run(Cmd(`$(Base.julia_cmd()) --version`; env=Dict("DYLD_FALLBACK_LIBRARY_PATH"=>join(paths[1:4],':'))))
+
+
+
 println("========================================")
 println("mpiexec(cmd -> run(`julia --version`))")
 println("========================================")
-mpiexec(cmd -> run(`julia --version`))
+mpiexec(cmd -> run(`$(Base.julia_cmd()) --version`))
 
 
 println("========================================")
 println("mpiexec(cmd -> run(`\$cmd -n 1 julia --version`))")
 println("========================================")
-mpiexec(cmd -> run(`$cmd -n 1 julia --version`))
+mpiexec(cmd -> run(`$cmd -n 1 $(Base.julia_cmd()) --version`))
 
 
